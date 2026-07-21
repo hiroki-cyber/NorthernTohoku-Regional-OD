31d9440f33a39c6a9c9e5b138f2809402a648c7c
hiroki-cyber <hirokimatukura@gmail.com>
2026-07-21 23:22:26 +0900

    Fix vite config build path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NorthernTohoku-Regional-OD/', // ← これを追加
+  base: '/NorthernTohoku-Regional-OD/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'), // ★ 明示的に index.html の絶対パスを指定
+      },
+    },
+  },
 });
